d94100b052c8aba998f1eaff8f0df8eb0dcd2fc7
Daria Moreno-Gogoleva <iwanttobealight@yandex.ru>
2023-03-19 19:40:06 +0300

    Initial Commit

---
+++ BlasterGameMacApp/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  BlasterGameMacApp
+//
+//  Created by Daria Moreno-Gogoleva on 19.03.2023.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
